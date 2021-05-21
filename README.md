df553ff6d8b5c40091d2eec04eb15f5a3acf5380
urnisha19 <urnisha09@gmail.com>
2021-05-21 16:31:46 +0600

    complete

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://glacial-bayou-10112.herokuapp.com/ deployed to Heroku
 
-# complete-website-server-urnisha19
+# staffIT-server-urnisha19
 
--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs-extra');
 const fileUpload = require('express-fileupload');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-
 const app = express();
+require('dotenv').config()
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('services'));
 app.use(fileUpload());
 
-const port = 5000;
-
+const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rzg3r.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -24,84 +22,78 @@ client.connect(err => {
   const reviewCollection = client.db("StaffIT").collection("review");
   const orderCollection = client.db("StaffIT").collection("order");
 
-  console.log("DB connected");
-
+  // <----------Get Methods---------->
   //Server Connection Test
   app.get('/', (req, res) => {
     res.send('Hello!!!!! Server is working!');
   });
+  console.log("DB connected");
 
+  //Admin Page all Orders  
+  app.get('/admin/orderList', (req, res) => {
+    orderCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  //Homepage all added Services 
+  app.get('/services', (req, res) => {
+    serviceCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  });
+
+  //get all added Admin
+  app.get('/showAllAdmin', (req, res) => {
+    adminCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  //Homepage get all review  
+  app.get('/review', (req, res) => {
+    reviewCollection.find({})
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  //get all orders of a customer  
+  app.get('/order', (req, res) => {
+    orderCollection.find({ email: req.query.email })
+      .toArray((err, documents) => {
+        res.send(documents);
+      })
+  })
+
+  // <----------Post Methods---------->
   //Admin Page addService
   app.post('/admin/addService', (req, res) => {
     const file = req.files.file;
     const title = req.body.title;
     const description = req.body.description;
-
     const newImg = file.data;
     const encImg = newImg.toString('base64');
-
     var image = {
       contentType: file.mimType,
       size: file.size,
       img: Buffer.from(encImg, 'base64')
     };
-
     serviceCollection.insertOne({ title, description, image })
       .then(result => {
         res.send(result.insertedCount > 0);
       })
   })
 
-  //get all added Services to show in homepage
-  app.get('/services', (req, res) => {
-    serviceCollection.find({})
-      .toArray((err, documents) => {
-        return res.send(documents);
-      })
-  });
-
-  //Admin Page Customer Order List show 
-  app.get('/admin/orderList', (req, res) => {
-    orderCollection.find({})
-      .toArray((err, documents) => {
-        return res.send(documents);
-      })
-  })
-
   //Admin Page Make Admin
   app.post('/admin/makeAdmin', (req, res) => {
-    const makeAdmin = req.body;
-    adminCollection.insertOne(makeAdmin)
-      .then(result => {
-        res.send(result.insertedCount > 0);
-      })
-  })
-
-  // show all added Admin
-  app.get('/showAllAdmin', (req, res) => {
     const email = req.body.email;
-    console.log(email);
-    adminCollection.find({ email: email })
-      .toArray((err, documents) => {
-        res.send(documents.length > 0);
-      })
-  })
-
-  //Customer page addOrder 
-  app.post('/customer/serviceBooking', (req, res) => {
-    const order = req.body;
-    console.log(order)
-    orderCollection.insertOne(order)
+    adminCollection.insertOne({ email })
       .then(result => {
         res.send(result.insertedCount > 0);
-      })
-  })
-
-  // show all orders of a customer  
-  app.get('/order', (req, res) => {
-    orderCollection.find({ email: req.query.email })
-      .toArray((err, documents) => {
-        res.send(documents);
       })
   })
 
@@ -110,21 +102,24 @@ client.connect(err => {
     const name = req.body.name;
     const companyName = req.body.companyName;
     const description = req.body.description;
-
     reviewCollection.insertOne({ name, companyName, description })
       .then(result => {
         res.send(result.insertedCount > 0);
       })
   })
 
-  //get all review to show in homepage
-  app.get('/review', (req, res) => {
-    reviewCollection.find({})
-      .toArray((err, documents) => {
-        return res.send(documents);
+  //Customer page addOrder 
+  app.post('/customer/serviceBooking', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const service = req.body.service;
+    // const price = req.body.price;
+    // const status = req.body.status;
+    orderCollection.insertOne({ name, email, service })
+      .then(result => {
+        res.send(result.insertedCount > 0)
       })
   })
-
 });
 
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || 5000)
